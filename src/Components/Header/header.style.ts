c45d05ebd2f0633ced7598027b25f3465542b7de
Camilo Andres Gutierrez <Camilo Andres>
2022-11-29 19:17:43 -0500

    Finished Header behavior

--- src/Components/Header/header.style.ts
+++ src/Components/Header/header.style.ts
@@ -1,26 +1,27 @@
 import { NavLink } from 'react-router-dom';
-import styled, { AnyStyledComponent } from 'styled-components';
+import styled, { AnyStyledComponent, css } from 'styled-components';
 import { sizes } from '../../../utils/screenSizes';
 
 export const HeaderContainer = styled.header`
   width: 100%;
-  height: 50px;
   background-color: #fff;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid gainsboro;
+  position: relative;
 `;
 
 export const Link = styled(NavLink)`
   width: fit-content;
   font-weight: normal;
   text-decoration: none;
-  /* color: black; */
+  color: black;
   cursor: pointer;
-  /* &:hover {
+  transition: all ease-in-out 300ms;
+  &:hover {
     color: #3b7197;
-  } */
+  }
 `;
 
 export const TitleHeader = styled.li`
@@ -34,9 +35,106 @@ export const Menu = styled.nav`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  width: inherit;
+  width: 70%;
 
-  @media screen and (max-width: ${sizes.mobileL}){
+  @media screen and (max-width: ${sizes.tablet}){
+    display: none;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  width: 30%;
+  padding: 10px 20px;
+  img {
+    width: 200px;
+  }
+
+  @media screen and (max-width: ${sizes.tablet}){
+    width: 50%;
+    padding: 5px;
+  }
+`;
+
+export const MobileMenu = styled.div<{ isActive: boolean }>`
+  position: fixed;
+  width: 0;
+  height: 100vh;
+  z-index: 999;
+  top: 0;
+  right: 0;
+  background-color: white;
+  transition: all ease-in-out 500ms;
+  box-shadow: 10px 10px 10px 10px rgba(0,0,0,0.5);
+  visibility: hidden;
+
+  ${props => props.isActive && css`
+    width: 80vw;
+    visibility: visible;
+  `}
+
+  .exit-menu--container {
+    height: 78px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 25px;
+    cursor: pointer;
+
+    > div {
+      width: 100px;
+      display: flex;
+      justify-content: center;
+      visibility: visible;
+      transition: all ease-in-out 400ms;
+      ${props => !props.isActive && css`
+        visibility: hidden;
+      `}
+    }
+  }
+
+  li {
+    display: block;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    cursor: pointer;
+    border-radius: 10px;
+    transition: all 200ms;
+
+    :hover {
+      background-color: rgb(159, 31, 0);
+      color: white !important;
+    }
+  }
+
+  .links--container {
+    width: 95%;
+    margin: auto;
+    border-radius: 10px;
+  }
+`;
+
+export const HamburgerMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100px;
+
+  section {
+    cursor: pointer;
+  }
+
+  div {
+    width: 25px;
+    height: 2px;
+    background-color: black;
+    margin-bottom: 5px;
+  }
+
+  @media screen and (min-width: 769px){
     display: none;
   }
 
