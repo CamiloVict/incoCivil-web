5871ebf70fb8812395bdb322e0828069108f0c59
Camilo Victoria <cvictoria@vovsolutions.com>
2022-12-22 14:42:03 -0500

    Buissines ID format fixed

--- src/https/comments.ts
+++ src/https/comments.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyALaQ46AviymyKBKe6SUzfR29Lcmd3zBho';
 // const ID ='Incocivil+Insumos+Para+La+Construccion+Civil+Sas/@3.433811,-76.5284588,17z/data=!3m1!4b1!4m5!3m4!1s0x8e30a68d572a04d9:0xee211c8a1e110edb!8m2!3d3.4338734!4d-76.5262723?hl=es'
-const ID ='4063-0323-4073-8296-02'
+const ID ='406303234073829602'
 
 export async function getBusinessComments(businessId: string, apiKey: string) {
 
